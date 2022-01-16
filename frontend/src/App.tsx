0306184f1485332fd7d27bc5f174e7e1f041de40
ThiagoTassinari <thiagotassinari12@gmail.com>
2022-01-16 10:33:33 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-    <h1>Qualquer coisa</h1>
+    <Navbar />
   );
 }
 
---
+++ frontend/src/dist/App.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var Navbar_1 = require("components/Navbar");
+function App() {
+    return (React.createElement(Navbar_1["default"], null));
+}
+exports["default"] = App;
