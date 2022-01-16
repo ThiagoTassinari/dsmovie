14316b93fbcb1aa4cd24ae53a51cc753497f1e0a
ThiagoTassinari <thiagotassinari12@gmail.com>
2022-01-16 08:33:58 -0300

    Project create

---
+++ backend/src/main/java/com/devsuperior/srmovie/SrmovieApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.srmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/srmovie/SrmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.srmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
